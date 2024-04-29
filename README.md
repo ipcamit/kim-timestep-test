b9df846b28bc51a00b0d9191ddcf4f3ec5b93e96
Amit Gupta <amit_@outlook.in>
2024-04-29 13:46:45 -0500

    Langevin stabilty test running, ver 1

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-Langevin maxtime test
-=====================
+Langevin MD Timestep Test
+=========================
+
+This test runs an overdamped Langevin dynamics simulation. The test quantity is then the average temperature of the system. The test passes if the average temperature is within a certain tolerance of the target temperature. 
+
+The damping constant is determined by scaling the damping time by the mean atomic mass units of the simulation cell. This provides a reasonable damping constant for the simulation cell, as LAMMPS apply the damping constant in `fix langevin` to scale the momentum term as `(m/damping) v`. Therefore for  any simulation cell the damping will now effectively be scaled as `((m/mean_mass)/damping) v`, providing somewhat equal footing.
+
+Other defaults and assumptions are listed below, and can be modified by searching for the `Parameter` in the `devel.py` file.
+
+Defaults:
+
+| Parameter | Value | Description |
+| --------- | ----- | ----------- |
+| `timesteps` | 1e-6 to 1e-3 fs | timesteps to tests simulations on |
+| `temps` | 1e1 to 1e4 K | target temperature of the simulation |
+| `n_atoms` | 250 | The desired number of atoms in simulation cell, it influences the parameter `repl`, which sets the replicatio of the cell, such that total number of particles are ~250 |
+| `n_steps` | 1000 | Number of steps to run the simulation for |
+| `variance_tol` | 0.2 | Tolerance for the average temperature fluctuation, 0.2 means, 0.8 * temp < temps < 1.2 * temp, where temp is the requested temperature |
+| `langevin_damp` | 10.0 | timesteps to avgerage over for Langevin damping constant |
+
+
+## Property defination
+This test calculates two properties namely, `maximum-stable-timestep`, and
+  `all-stable-timestep-per-temperature`. The `maximum-stable-timestep` is the absolute largest timestep that can run the simulation stably, irrespective of the temperature. this serves as a scalar value for comparing the stability of the simulation. The `all-stable-timestep-per-temperature` lists the combinations of all temperatures and timestemps for which the simulaitons ran stably. This serves as a detailed information on the stability of the simulation.
--- devel.py
+++ devel.py
@@ -11,81 +11,74 @@ from ase import Atoms
 from ase.io.lammpsdata import write_lammps_data
 
 class MaximumStableLangevinTimestep(CrystalGenomeTest):
+    """
+    This test calculates the maximum stable Langevin timestep for a given model, as a proxy for the model stability. Additionally it calculates the stable timestep for a range of temperatures.
+    """
     def _calculate(self, structure_index: int):
         """
         structure_index:
             KIM tests can loop over multiple structures (i.e. crystals, molecules, etc.). 
             This indicates which is being used for the current calculation.
         """
-        times=np.array([0.000001, 0.00001, 0.0001, 0.001])
+        # Define the timesteps and temperatures to test
+        timesteps=np.array([0.000001, 0.00001, 0.0001, 0.001])
         temps=np.array([10.,100.,1000.,10000.])
-        # f = open("debug.txt", "w")
-        f = sys.stdout
-        atoms = self.atoms[structure_index]
 
+        atoms = self.atoms[structure_index]
         species_list = list(set(atoms.get_chemical_symbols()))
 
-        crystal_lammps_file = f"{str(atoms.get_chemical_formula())}.lammps"
-        write_lammps_data(crystal_lammps_file, atoms, species_list)
-        f.write(f"Written lammps data file: {crystal_lammps_file}\n")
-        
+        # Write the LAMMPS data file for the crystal
+        crystal_lammps_file = f"{str(atoms.get_chemical_formula())}.dat"
+        write_lammps_data(crystal_lammps_file, atoms, species_list)        
 
         stable_times_all = []
+
+        # Loop over the temperatures
         for temp in temps:
-            lammps_file = self.generate_lammps_file(atoms, species_list, temp, times) 
-            lammps_file_name = f"{str(atoms.get_chemical_formula())}_{temp}.lammps"
+            # Generate the LAMMPS input file template
+            lammps_file_template = self.generate_lammps_file(atoms, species_list, timesteps) 
+            lammps_file_name = f"{str(atoms.get_chemical_formula())}.lammps"
             with open(lammps_file_name,"w") as lmp_f:
-                lmp_f.write(lammps_file)
+                lmp_f.write(lammps_file_template)
+
+            # Run LAMMPS for each temperature
             try:
-                result = subprocess.check_output(["lmp", "-in", lammps_file_name],stderr=subprocess.STDOUT)
+                result = subprocess.check_output(["lmp", "-in", lammps_file_name, "-var", "temp_sim", str(temp)],stderr=subprocess.STDOUT)
             except subprocess.CalledProcessError as e:
                 print(f"Failed to run LAMMPS {e}")
                 result = e.output
 
-            f.write(f"SIM: file {lammps_file_name}\n")
-
-            # Parse the result
+            # Parse the result to get the stable times
             if result is not None:
-                stable_times = self.parse_lammps_result(result, f"{str(atoms.get_chemical_formula())}_{temp}_temp.dat",  temp, times)
+                stable_times = self.parse_lammps_result(result, f"{str(atoms.get_chemical_formula())}_{temp}_temp.dat",  temp, timesteps)
                 stable_times_all.extend(stable_times)
-            f.write(f"Stable times for temperature {temp}: {stable_times}\n")
 
+        # Get the maximum stable time
         stable_times_all = np.array(stable_times_all)
-        f.write(f"Stable times: {stable_times_all}\n")
         max_stable_time = np.max(stable_times_all[:,1])
         
-        # self._add_property_instance("binding-energy-relation-crystal")
+        # Add the results to the property instance
         self._add_property_instance("maximum-stable-langevin-timestep")
-
         self._add_common_crystal_genome_keys_to_current_property_instance(structure_index,write_stress=False,write_temp=False) # last two default to False
-
-        # self._add_key_to_current_property_instance("average-wigner-seitz-radius",average_wigner_seitz_radius,"angstrom")
-        print("Maximum stable timestep: ", max_stable_time)
-
         self._add_key_to_current_property_instance("maximum-stable-timestep",max_stable_time, "ps")
-
-        print("Maximum stable timestep: ", max_stable_time)
-
-        self._add_key_to_current_property_instance("maximum-stable-timestep-per-temperature",stable_times_all)
-        # with open("debug"+str(structure_index)+".txt","w") as f:
-        #     for r,u in zip(average_wigner_seitz_radius,binding_potential_energy_per_atom):
-        #         f.write(str(r)+" "+str(u)+"\n")
+        self._add_key_to_current_property_instance("all-stable-timestep-per-temperature",stable_times_all)
 
     def generate_lammps_file(self,
                         atoms:Atoms,
                         species_list:list,
-                        temp:float,
-                        times:np.ndarray, 
-                        nsteps:int = 100,
+                        timesteps:np.ndarray, 
+                        n_steps:int = 100,
                         repl:int = 6) -> str:
-
+        """
+        Generate the LAMMPS input file for the test
+        """
         file_str = f"# LAMMPS input script for testing simulation stability \n"
         file_str += f"# for the {self.model_name} model\n"
         file_str += f"\n\n"
         file_str += f"kim init {self.model_name}  metal\n\n"
 
         # read datafile
-        file_str += f"read_data {str(atoms.get_chemical_formula())}.lammps\n\n"
+        file_str += f"read_data {str(atoms.get_chemical_formula())}.dat\n\n"
 
         file_str += f"kim interactions {' '.join(species_list)}\n"
         for i, s in enumerate(species_list):
@@ -94,48 +87,51 @@ class MaximumStableLangevinTimestep(CrystalGenomeTest):
         file_str += "neighbor 1.0 bin\n"
         file_str += "neigh_modify every 1 delay 0 check no one 1000000 page 10000000\n\n"
 
-        n_atoms = atoms.get_global_number_of_atoms()
+        total_atoms = atoms.get_global_number_of_atoms()
+        n_atoms = 250
         # ~ 250 atoms is the aim right now
-        repl = int((250/n_atoms)**(1/3))
+        repl = int((n_atoms/total_atoms)**(1/3))
         if repl < 1:
             repl = 1
 
         file_str += f"replicate {repl} {repl} {repl}\n\n"
 
         file_str += f"variable temp equal temp\n"
-        file_str += f"fix 2 all ave/time 1 1 1 v_temp ave one file {atoms.get_chemical_formula()}_{temp}_temp.dat\n"
+        file_str += f"fix 1 all ave/time 1 1 1 v_temp ave one file {atoms.get_chemical_formula()}_${{temp_sim}}_temp.dat\n"
 
-        file_str += f"velocity all create {temp} 87287 rot yes dist gaussian\n"
+        file_str += "velocity all create ${temp_sim} 87287 rot yes dist gaussian\n"
 
-        # TODO: Use Langevin thermostat
-        file_str += f"fix 1 all nvt temp {temp} {temp} $(1.0*dt)\n\n"
+        file_str += f"fix 2 all nve\n"
+        # langevin dynamics with a damping constant of 10 fs. The damping is scaled by the average atomic mass of the system.
+        # This provides a reasonable damping for a wide range of systems.
+        langevin_damp = 10.0
+        file_str += f"fix 3 all langevin ${{temp_sim}} ${{temp_sim}}  {langevin_damp * np.mean(atoms.get_atomic_numbers())} 1234567\n\n"
         file_str += f"thermo 1\n"
 
-        for i, t in enumerate(times):
+        for i, t in enumerate(timesteps):
             file_str += f"timestep {t}\n"
-            file_str += f"run {nsteps}\n\n"
+            file_str += f"run {n_steps}\n\n"
 
             file_str += f"print \"<<SUCCESS>> <<{t}>>\"\n" # success
 
         return file_str
 
-    def parse_lammps_result(self, output, temp_file, temp, time,  variance_tol=0.5, nsteps=100):
+    def parse_lammps_result(self, output, temp_file, temp, time,  variance_tol=0.2, n_steps=100):
+        """
+        Parse the LAMMPS output to get the stable times
+        """
         temp_arr = np.loadtxt(temp_file, skiprows=2)
-        n_successful_simulations = temp_arr.shape[0] // nsteps
-        print(n_successful_simulations,temp_arr.shape[0], nsteps)
+        n_successful_simulations = temp_arr.shape[0] // n_steps
         stable_times = []
         for i in range(n_successful_simulations):
-            t = np.mean(temp_arr[i*nsteps: (i+1)*nsteps,1])
-            print(t, temp, variance_tol * temp)
+            t = np.mean(temp_arr[i*n_steps: (i+1)*n_steps,1])
             if abs(t-temp) < variance_tol * temp:
                 stable_times.append([temp, time[i]])
             else:
-                print(f"Rejecting simulation as temperature variance too high for T = {temp} and time = {time[i]}, mean temp = {t}.")
+                # print(f"Rejecting simulation as temperature variance too high for T = {temp} and time = {time[i]}, mean temp = {t}.")
+                pass
         return stable_times
 
-# This queries for equilibrium structures in this prototype and builds atoms
-# test = BindingEnergyVsWignerSeitzRadius(model_name="MEAM_LAMMPS_KoJimLee_2012_FeP__MO_179420363944_002", stoichiometric_species=['Fe','P'], prototype_label='AB_oP8_62_c_c')
-                
 # Alternatively, for debugging, give it atoms object or a list of atoms objects
 atoms = bulk('NaCl','rocksalt',a=4.58)
 test = MaximumStableLangevinTimestep(model_name="LennardJones612_UniversalShifted__MO_959249795837_003", atoms=atoms)
